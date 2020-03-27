bb01a60cffa223bf1bc9d012f89d3e1d09c0cfe4
Christos <constantinides.ch@gmail.com>
2020-03-27 15:06:34 +0200

    JSON quotes.

--- src/components/VideoChat.js
+++ src/components/VideoChat.js
@@ -28,8 +28,8 @@ const VideoChat = () => {
           room: roomName
         }),
         headers: {
-          "Accept": "application/json;  charset=UTF-8",
-          "Content-Type": "application/json; charset=UTF-8"
+          "Accept": "application/json",
+          "Content-Type": "application/json"
         }
       }).then(res => res.json());
       setToken(data.token);
